0e57491a86a50ce024f5d68c4f949b144772d54f
Dimitar Atanassov <dimitara@gmail.com>
2016-09-13 19:57:53 +0300

    arsis map with municipalities

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@
     var g = null;
     var projection = null;
     var selected_province = null;
+    var municipalities = [];
 
     function region_clicked(d) {
         var x, y, k;
@@ -15,7 +16,7 @@
             y = centroid[1];
             k = 2.5;
             centered = d;
-            zoom_in(d.properties.nuts3);
+            zoom_in(d.properties.nuts3, k, x, y);
         } else {
             x = width / 2;
             y = height / 2;
@@ -25,25 +26,37 @@
             zoom_out();
         }
 
-        g.transition()
-            .duration(750)
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-            .style("stroke-width", 1.5 / k + "px");
+        
     } 
 
-    function zoom_in(province) {
+    function zoom_in(province, k, x, y) {
         if(selected_province) {
             d3.selectAll('.village.' + selected_province).classed('visible', false);
+            zoom_out();
+            
+            return ;
         }
 
         selected_province = province;
         d3.selectAll('.town').classed('visible', false);
         d3.selectAll('.village.' + province).classed('visible', true);
+        d3.selectAll('.stats').classed('hidden', true);
+
+        g.transition()
+            .duration(750)
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
     }
 
     function zoom_out() {
         d3.selectAll('.municipality.show')
                     .classed('show', false);
+        d3.selectAll('.village.' + selected_province).classed('visible', false);
+        d3.selectAll('.stats').classed('hidden', false);
+
+        selected_province = null;
+        g.transition()
+            .duration(750)
+            .attr("transform", "translate(0,0)");
     }
 
     function load_provinces() {
@@ -81,21 +94,31 @@
                     return d.properties.nuts3; 
                 })
                 .on('click', function(d) {
+                    if(['SHU', 'VAR', 'BGS'].indexOf(d.properties.nuts3) === -1) {
+                        zoom_out();
+                        return ;
+                    }
+
                     d3.selectAll('.municipality.show')
                         .classed('show', false);
                     
                     d3.selectAll('.nuts-' + d.properties.nuts3)
                         .attr('class', 'municipality show nuts-' + d.properties.nuts3);
 
-                    region_clicked(d); //d3.select('#' + d.properties.nuts3 + ' > path'));
+                    region_clicked(d); 
                 })
                 .attr('class', 'province')
                 .append(
                     "path")
                 .attr("d", path)
                 .style("stroke-width", "1")
-                .style("stroke", "#7c9944")
-                .attr('fill', 'rgba(255,255,255,.45)');
+                .style("stroke", "#3d322a")
+                .attr('fill', function(d) {
+                    if(['SHU', 'VAR', 'BGS'].indexOf(d.properties.nuts3) > -1) {
+                        return '#b7c798';
+                    }
+                    return 'rgba(255,255,255,.45)'; 
+                });
 
             load_places();    
             load_stats();
@@ -103,7 +126,7 @@
     }
 
     function load_municipalities() {
-        d3.json("geojson/municipalities.json", function(json) {
+        d3.json("geojson/municipalities.json?1", function(json) {
             // create a first guess for the projection
             var center = d3.geo.centroid(json)
             var scale = 120;
@@ -129,11 +152,16 @@
             projection = d3.geo.mercator().center(center)
                 .scale(scale).translate(offset);
             path = path.projection(projection);
+            json.features.forEach(function(f) {
+                if(f.properties.nuts4 === 'SHU23') {
+                    console.log('whhaaat', f);
+                }
+            });
 
-            g.selectAll("path").data(json.features).enter()
+            g.selectAll("text").data(json.features).enter()
                 .append('g')
                 .attr('id', function(d){
-                    return d.properties.nut4; 
+                    return d.properties.nuts4; 
                 })
                 .attr('class', function(d) {
                     return 'municipality nuts-' + d.properties.nuts3; 
@@ -142,22 +170,26 @@
                     "path")
                 .attr("d", path)
                 .style("stroke-width", "1")
-                .style("stroke", "#7c9944")
+                .style("stroke", "rgba(61, 50, 42, 0.3)")
                 .attr('fill', 'rgba(255,255,255,.45)');
         });
     }
 
     function load_places() {
-        d3.json("geojson/places.json?4", function(json) {
+        d3.json("geojson/places.json?5", function(json) {
             json.forEach(function(place) {
+                if(place.village) {
+                    municipalities.push(place.municipality_code);
+                }
+
                 var geo = place.geo.split(',');
                 var placeGroup = g.append('g');
                 
                 placeGroup
-                    .attr('id', place.municipality_code)
+                    .attr('id', place.ekatte)
                     .attr("class", function(d) {
                         if(place.village === 0) {
-                            return 'town visible';
+                            return 'town';
                         }
 
                         if(place.village === 1) {
@@ -227,8 +259,126 @@
     }
 
     function load_stats() {
-       var shuBox = d3.select('#SHU').getClientBoundingBox(); 
+        var shuBox = d3.select('#SHU')[0][0].getBBox(); 
+        var varBox = d3.select('#VAR')[0][0].getBBox(); 
+        var bgsBox = d3.select('#BGS')[0][0].getBBox(); 
+        
+        var gShu = g.append('g')
+            .attr('id', 'SHU-stats')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('transform', 'translate(' + (shuBox.x + shuBox.width/2) + ',' + (shuBox.y + shuBox.height/2) + ')')
+            .attr('class', 'stats');
+        
+        var gVar = g.append('g')
+            .attr('id', 'VAR-stats')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('transform', 'translate(' + (varBox.x + varBox.width/2) + ',' + (varBox.y + varBox.height/2) + ')')
+            .attr('class', 'stats');
 
+        var gBgs = g.append('g')
+            .attr('id', 'BGS-stats')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('transform', 'translate(' + (bgsBox.x + bgsBox.width/2) + ',' + (bgsBox.y + bgsBox.height/2) + ')')
+            .attr('class', 'stats');
+
+        gShu.append('circle')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('r', 50)
+            .attr('fill', '#3d322a')
+            .attr('opacity', 0.4);
+
+        gShu.append('text')
+            .text('Шумен')
+            .attr('fill', '#fff')
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2; 
+            });
+        
+        gShu.append('text')
+            .text('323')
+            .attr('fill', '#fff')
+            .attr('font-size', 24)
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2 + 35; 
+            });
+
+        gVar.append('circle')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('r', 50)
+            .attr('fill', '#3d322a')
+            .attr('opacity', 0.4);
+        
+        gVar.append('text')
+            .text('Варна')
+            .attr('fill', '#fff')
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2; 
+            });
+        
+        gVar.append('text')
+            .text('576')
+            .attr('fill', '#fff')
+            .attr('font-size', 24)
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2 + 35; 
+            });
+
+        gBgs.append('circle')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('r', 50)
+            .attr('fill', '#3d322a')
+            .attr('opacity', 0.4);
+        
+        gBgs.append('text')
+            .text('Бургас')
+            .attr('fill', '#fff')
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2; 
+            });
+        
+        gBgs.append('text')
+            .text('228')
+            .attr('fill', '#fff')
+            .attr('font-size', 24)
+            .attr('x', function() {
+                return -this.getBBox().width/2; 
+            })
+            .attr('y', function() {
+                return -this.getBBox().height/2 + 35; 
+            });
+
+    }
+
+    function bind_notes() {
+    }
+
+    function mark_municipalities() {
+        municipalities.forEach(function(m) {
+            console.log(m);
+            d3.selectAll('#' + m + ' > path').
+                attr('fill', '#7c9944')
+        });
     }
 
     function init() {
@@ -239,6 +389,10 @@
 
         load_provinces();
         load_municipalities();
+
+        setTimeout(function() {
+            mark_municipalities();
+        }, 1000);
     }
 
     init();
